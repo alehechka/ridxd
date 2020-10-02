3162ff3d1e78fe764db927ea003a43c0b62b0306
alehechka <42357034+alehechka@users.noreply.github.com>
2020-10-02 10:39:00 -0500

    Prepping Release (1.0.0)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-export default (map: { [key: string]: Record<any, any>}, key: string) => {
+export default (map: { [key: string]: Record<any, any> }, key: string) => {
   return Object.keys(map).reduce((arr, index) => {
     arr.push({
       ...map[index],
-      [key]: index
+      [key]: index,
     });
     return arr;
-  }, [] as Array<Record<any, any>>)
-}
+  }, [] as Array<Record<any, any>>);
+};
